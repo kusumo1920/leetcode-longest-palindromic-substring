d30210ce6a9804c177b0ff78babde7b9815e2891
Kesit Budi Kusumo <kesit@kernel.id>
2024-05-19 22:19:52 +0700

    feat: add solutions using brute force and dp approaches
    O(n^2)

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/kusumo1920/leetcode-longest-palindromic-substring
+
+go 1.22.2
---
+++ main.go
@@ -0,0 +1,95 @@
+package main
+
+import "fmt"
+
+func main() {
+	s := "babad"
+	output := longestPalindromeSolution3(s)
+	fmt.Println(output)
+}
+
+func longestPalindromeSolution1(s string) string {
+	var resultSubstring string
+
+	for i := 0; i < len(s); i++ {
+		for j := i; j < len(s); j++ {
+			currentSubstring := s[i : j+1]
+			if isPalindrome(currentSubstring) && len(currentSubstring) > len(resultSubstring) {
+				resultSubstring = currentSubstring
+			}
+		}
+	}
+
+	return resultSubstring
+}
+
+func isPalindrome(s string) bool {
+	for i := 0; i < len(s)/2; i++ {
+		if s[i] != s[len(s)-1-i] {
+			return false
+		}
+	}
+
+	return true
+}
+
+func longestPalindromeSolution2(s string) string {
+	check := func(i, j int) bool {
+		left := i
+		right := j - 1
+
+		for left < right {
+			if s[left] != s[right] {
+				return false
+			}
+
+			left++
+			right--
+		}
+
+		return true
+	}
+
+	for length := len(s); length > 0; length-- {
+		for start := 0; start < len(s)-length; start++ {
+			if check(start, start+length) {
+				return s[start : start+length]
+			}
+		}
+	}
+
+	return ""
+}
+
+func longestPalindromeSolution3(s string) string {
+	n := len(s)
+	dp := make([][]bool, n)
+	for i := range dp {
+		dp[i] = make([]bool, n)
+	}
+	ans := []int{0, 0}
+
+	for i := 0; i < n; i++ {
+		dp[i][i] = true
+	}
+
+	for i := 0; i < n-1; i++ {
+		if s[i] == s[i+1] {
+			dp[i][i+1] = true
+			ans = []int{i, i + 1}
+		}
+	}
+
+	for diff := 2; diff < n; diff++ {
+		for i := 0; i < n-diff; i++ {
+			j := i + diff
+
+			if s[i] == s[j] && dp[i+1][j-1] {
+				dp[i][j] = true
+				ans = []int{i, j}
+			}
+		}
+	}
+
+	return s[ans[0] : ans[1]+1]
+}
